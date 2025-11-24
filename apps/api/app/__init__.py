52d2e84b5defbb0e8e75ac17f5d3af8bb414c8f6
astheria <octa.toriq@gmail.com>
2025-11-25 00:26:49 +0700

    feat(migrations): add drone_id column to missions table with foreign key constraint

--- apps/api/app/__init__.py
+++ apps/api/app/__init__.py
@@ -1,14 +1,13 @@
 from flask import Flask
 from flask_smorest import Api
 from .config import Config
-from .extensions import db,migrate,jwt,cors
+from .extensions import db, migrate, jwt, cors
+from .resources.mission import blp as MissionBlueprint
+from .resources.auth import blp as AuthBlueprint
 from .resources.fleet import blp as FleetBlueprint
-from resources.mission import blp as MissionBlueprint
-
 
 def create_app():
     app = Flask(__name__)
-    
     cors.init_app(app)
 
     app.config.from_object(Config)
@@ -37,16 +36,9 @@ def create_app():
 
     api = Api(app)
 
-    from . import models
-
-    from .resources.auth import blp as AuthBlueprint
-    from .resources.fleet import blp as FleetBlueprint
-
+    from . import models  
 
     api.register_blueprint(AuthBlueprint)
     api.register_blueprint(FleetBlueprint)
     api.register_blueprint(MissionBlueprint)    
-    
     return app
-
-    
